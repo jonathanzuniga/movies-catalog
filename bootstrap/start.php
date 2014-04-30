3483d141d944033c22e672e73addf2feea9c3e52
Jonathan Zúñiga <jonathanzuniga@gmail.com>
2014-04-30 12:50:59 -0700

    Add local hostname

--- bootstrap/start.php
+++ bootstrap/start.php
@@ -26,7 +26,7 @@ $app = new Illuminate\Foundation\Application;
 
 $env = $app->detectEnvironment(array(
 
-	'local' => array('LuisAngel-HP', 'your-machine-goes-here'),
+	'local' => array('LuisAngel-HP', 'Estanislaos-Mac-mini.local'),
 
 ));
 
